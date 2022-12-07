800a281d8e7b0d85a526d6923d6f0c80e14b41bf
wxxk <dwde22@gmail.com>
2022-12-07 17:21:35 +0900

    feat : mbti / create [이상욱]

--- MBTI/models.py
+++ MBTI/models.py
@@ -2,7 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Mbti(models.Model):
+
+    MBTI_CHOICES = {
+        ('INFP', 'INFP'),
+        ('ENFP', 'ENFP'),
+        ('ESFJ', 'ESFJ'),
+        ('ISFJ', 'ISFJ'),
+        ('ISFP', 'ISFP'),
+        ('ESFP', 'ESFP'),
+        ('INTP', 'INTP'),
+        ('INFJ', 'INFJ'),
+        ('ENFJ', 'ENFJ'),
+        ('ENTP', 'ENTP'),
+        ('ESTJ', 'ESTJ'),
+        ('ISTJ', 'ISTJ'),
+        ('INTJ', 'INTJ'),
+        ('ISPT', 'ISTP'),
+        ('ESTP', 'ESTP'),
+        ('ENTJ', 'ENTJ'),
+    }
+
     board = models.IntegerField()
-    mbti = models.CharField(max_length=100)
+    mbti = models.CharField(max_length=50, choices=MBTI_CHOICES, null=False)
     title = models.CharField(max_length=200)
     content= models.TextField()
