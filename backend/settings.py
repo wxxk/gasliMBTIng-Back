34d98e8b9b4a021e99749deedad011befe2405f6
rhoeunbin <rhoeb21@gmail.com>
2022-12-08 04:02:33 +0900

    feat : accounts/ 이미지 필드 추가 [노은빈]

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,9 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 이미지 media
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -159,3 +162,4 @@ DJOSER = {
          'user': 'accounts.serializers.UserSerializer',
     }
 }
+
