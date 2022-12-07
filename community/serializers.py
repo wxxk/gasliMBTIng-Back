8a3940cbf93a15e3457d25ad2de58086763a86f5
wooyeol <dnduf158@gmail.com>
2022-12-07 17:40:17 +0900

    fix : community/serializers 오류 수정 [이우열]

--- community/serializers.py
+++ community/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Community, Comment, Photo
+from accounts.serializers import UserSerializer
 
 
 class CommunitySerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source="user.username")
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Community
