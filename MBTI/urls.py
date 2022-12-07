7a07fde910833fa08a9c627979a8e4b27dd5f7cb
wxxk <dwde22@gmail.com>
2022-12-07 17:18:53 +0900

    feat : mbti / create [이상욱]

--- MBTI/urls.py
+++ MBTI/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
-from .views import mbti_list, mbti_details, mbti_create
+from .views import mbti_list, mbti_details
 
 
 urlpatterns = [
     path("", mbti_list),
-    path("create/", mbti_create),
     path("<int:pk>/", mbti_details),
 ]
